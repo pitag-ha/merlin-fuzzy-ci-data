17127ffbb9cd89379e95d6dafaf993d7acc763fc
Sonja Heinze <sonjaleaheinze@gmail.com>
2023-09-22 17:00:24 +0200

    Strip test code base down

--- test-code-base/irmin/src/irmin-pack-tools/tezos_explorer/import.ml
+++
@@ -1,5 +0,0 @@
-module Int63 = struct
-  include Optint.Int63
-
-  let t = Irmin.Type.int63
-end
--- test-code-base/irmin/test/irmin/data/test.ml
+++
@@ -1,6 +0,0 @@
-open! Import
-
-let () =
-  Random.self_init ();
-  Alcotest.run __FILE__
-    [ ("Fixed_size_string_set", Test_fixed_size_string_set.tests) ]
